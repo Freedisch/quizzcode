811f1556a447e85f6f0b6390e29ef95b948ff32a
Freedisch <freeproduc@gmail.com>
2023-07-13 08:27:07 +0300

    fix navbar
    Signed-off-by: Freedisch <freeproduc@gmail.com>

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,16 +5,15 @@ import { NavLink } from "react-router-dom";
 import { cn } from "@/lib/utils";
 import { Link as link2 } from "react-router-dom";
 const navigation = [
-  { title: "Home", path: "/home", disabled: false },
-  { title: "Add Oppotunities", path: "/opportunity", disabled: false },
-  { title: "FaQS", path: "/support" },
-  { title: "Contact us", path: "/contactUs", disabled: true },
+  { title: "Dashboard", path: "/home", disabled: false },
+  { title: "Quizzes", path: "/opportunity", disabled: false },
+  { title: "⭐ Give a star", path: "/support" },
 ];
 
-function Navbar() {
+export default function Navbar() {
   return (
     <div className="fixed backdrop-blur-sm bg-white/75 z-50 top-0 left-0 right-0 h-20 shadow-sm flex items-center justify-between">
-      <div className="container max max-w-7xl mx-auto w-full flex justify-between items-center">
+      <div className="items-center space-x2 md:flex">
         <Link
           href="/"
           className={buttonVariants({ variant: "link", size: "lg" })}
@@ -24,12 +23,13 @@ function Navbar() {
         <nav className="hidden gap-6 md:flex">
           {navigation.map((item, idx) => {
             return (
-              <li key={idx}>
-                <a href={item.path}>
-                  <Button variant="ghost" size="lg">
-                    {item.title}
-                  </Button>
-                </a>
+              <li
+                key={idx}
+                className={cn(
+                  "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm"
+                )}
+              >
+                <a href={item.path}>{item.title}</a>
               </li>
             );
           })}
@@ -38,5 +38,3 @@ function Navbar() {
     </div>
   );
 }
-
-export default Navbar;
